af6c6831f4c4fa8e987d8b118c328f371c26d433
Erfan Motallebi <Erfan.Motallebi1990@gmail.com>
2021-11-13 09:48:57 +0330

    added MongoDB to the Cluster and running

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import { errorHandler } from "./middlewares/errorHandler";
 import express, { Express, NextFunction, Request, Response } from "express";
 import "express-async-errors";
+import mongoose from "mongoose";
 // ! Auth Routers [ User ]
 import { currentUserRouter } from "./routes/authRouter/currentUser";
 import { signInRouter } from "./routes/authRouter/signin";
@@ -26,6 +27,24 @@ app.all("*", async (req: Request, res: Response) => {
 });
 app.use(errorHandler);
 
-app.listen(PORT, HOSTNAME, () => {
-  console.log(`Auth Service is running on - http://${HOSTNAME}:${PORT} - `);
-});
+// MongoDB and Listening Port
+const startApp = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv-cluster:27017", {
+      autoIndex: true,
+      dbName: "auth",
+      family: 4,
+      serverSelectionTimeoutMS: 10000,
+    });
+    console.log("⚒️   [ + ]  Database Connected ");
+  } catch (error) {
+    console.error(error);
+  }
+  app.listen(PORT, HOSTNAME, () => {
+    console.log(
+      `⚒️   [ + ]  Auth Service is running on - http://${HOSTNAME}:${PORT} - `
+    );
+  });
+};
+
+startApp();
