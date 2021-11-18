93ea916eeb783f03f8dd72894cbe73d7aa14c559
Erfan Motallebi <Erfan.Motallebi1990@gmail.com>
2021-11-18 08:41:32 +0330

    we splitted the index.js into 2 files to have the aid of ephemeral port using supertest

---
+++ auth/src/app.ts
@@ -0,0 +1,37 @@
+import { errorHandler } from "./middlewares/errorHandler";
+import express, { Express, NextFunction, Request, Response } from "express";
+import "express-async-errors";
+
+// ! Auth Routers [ User ]
+import { currentUserRouter } from "./routes/authRouter/currentUser";
+import { signInRouter } from "./routes/authRouter/signin";
+import { signOutRrouter } from "./routes/authRouter/signout";
+import { signUpRouter } from "./routes/authRouter/signup";
+import { NotFoundError } from "./errors/NotFoundError";
+import cookieSession from "cookie-session";
+
+const app: Express = express();
+app.set("trust proxy", 1);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+    name: "Micro-Kubect",
+  })
+);
+
+app.use(signUpRouter);
+app.use(signInRouter);
+app.use(signOutRrouter);
+app.use(currentUserRouter);
+
+// Error Handing
+app.use(async (req: Request, res: Response) => {
+  throw new NotFoundError();
+});
+app.use(errorHandler);
+
+export { app };
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,41 +1,8 @@
-import { errorHandler } from "./middlewares/errorHandler";
-import express, { Express, NextFunction, Request, Response } from "express";
-import "express-async-errors";
+import { app } from "./app";
 import mongoose from "mongoose";
-// ! Auth Routers [ User ]
-import { currentUserRouter } from "./routes/authRouter/currentUser";
-import { signInRouter } from "./routes/authRouter/signin";
-import { signOutRrouter } from "./routes/authRouter/signout";
-import { signUpRouter } from "./routes/authRouter/signup";
-import { NotFoundError } from "./errors/NotFoundError";
-import cookieSession from "cookie-session";
 
 const PORT = (process.env.PORT || 3000) as number;
 const HOSTNAME = (process.env.HOSTNAME || "localhost") as string;
-
-const app: Express = express();
-app.set("trust proxy", 1);
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-    name: "Micro-Kubect",
-  })
-);
-
-app.use(signUpRouter);
-app.use(signInRouter);
-app.use(signOutRrouter);
-app.use(currentUserRouter);
-
-// Error Handing
-app.use(async (req: Request, res: Response) => {
-  throw new NotFoundError();
-});
-app.use(errorHandler);
 
 // MongoDB and Listening Port
 const startApp = async () => {
