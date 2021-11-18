cd70ee73774ce286ea8c3cb3588ac8a97b711d65
Erfan Motallebi <Erfan.Motallebi1990@gmail.com>
2021-11-18 09:47:01 +0330

    added the inital setup for Test Envionrment

---
+++ auth/src/test/setup.ts
@@ -0,0 +1,24 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import { connect, connection } from "mongoose";
+
+let mongoMS: MongoMemoryServer;
+
+beforeAll(async () => {
+  mongoMS = await MongoMemoryServer.create();
+  const mongoMSUri: string = mongoMS.getUri();
+  await connect(mongoMSUri, {
+    dbName: "Microservice-Auth",
+  });
+});
+
+beforeEach(async () => {
+  const collections = await connection.db.collections();
+  collections.forEach((collection) => {
+    collection.deleteMany({});
+  });
+});
+
+afterAll(async () => {
+  mongoMS.stop();
+  await connection.close();
+});
