064dab8953d02132998b664dd927a47af9174c11
Erfan Motallebi <Erfan.Motallebi1990@gmail.com>
2021-12-04 10:14:47 +0330

    handled the errors from the post request to the singup route

--- client/pages/signup/index.js
+++ client/pages/signup/index.js
@@ -1,26 +1,31 @@
 import { useState } from "react";
 import axios from "axios";
+
 export default function singUp() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
 
   const submitHandler = async (e) => {
     e.preventDefault();
 
-    const response = await axios.request({
-      method: "POST",
-      url: "/api/users/signup",
-      data: {
-        email,
-        password,
-      },
-    });
-    console.log(response);
+    try {
+      await axios.request({
+        method: "POST",
+        url: "/api/users/signup",
+        body: {
+          email,
+          password,
+        },
+      });
+    } catch (err) {
+      setErrors(err.response.data.errors);
+    }
   };
 
   return (
     <div>
-      <div className="container">
+      <div className="container mt-5">
         <form onSubmit={submitHandler}>
           <div className="mb-3 row">
             <label htmlFor="staticEmail" className="col-sm-2 col-form-label">
@@ -52,6 +57,24 @@ export default function singUp() {
           </div>
           <button className="btn btn-primary">Submit</button>
         </form>
+        {errors.length > 0 && (
+          <section className="alert alert-danger my-2 p-4 ">
+            <div>
+              <ul className="list-group list-group-vertical-lg">
+                {errors.map((err, index) => {
+                  return (
+                    <li
+                      className="list-group-item list-group-item-warning mb-2 p-2 "
+                      key={index}
+                    >
+                      {err.message}
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          </section>
+        )}
       </div>
     </div>
   );
