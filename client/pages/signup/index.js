0bce0e98ab74de253b68201631d799df636b9609
Erfan Motallebi <Erfan.Motallebi1990@gmail.com>
2021-12-04 09:38:38 +0330

    added sigpup page

---
+++ client/pages/signup/index.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+export default function singUp() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log({ email, password });
+  };
+
+  return (
+    <div>
+      <div className="container">
+        <form onSubmit={submitHandler}>
+          <div className="mb-3 row">
+            <label htmlFor="staticEmail" className="col-sm-2 col-form-label">
+              Email
+            </label>
+            <div className="col-sm-10">
+              <input
+                type="text"
+                className="form-control"
+                id="staticEmail"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="mb-3 row">
+            <label htmlFor="inputPassword" className="col-sm-2 col-form-label">
+              Password
+            </label>
+            <div className="col-sm-10">
+              <input
+                type="password"
+                className="form-control"
+                id="inputPassword"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+              />
+            </div>
+          </div>
+          <button className="btn btn-primary">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+}
