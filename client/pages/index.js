5fc0f3cc0c434d461627ab117ada78e6d454b08f
Erfan Motallebi <Erfan.Motallebi1990@gmail.com>
2021-12-05 13:26:52 +0330

    added SSR pre-rendering Request

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,20 @@
-export default function Home() {
+import axios from "axios";
+
+export default function Home({ data }) {
+  console.log({ data });
   return (
     <div>
       <h1>Landing page</h1>
     </div>
   );
+}
+
+export async function getServerSideProps(ctx) {
+  const resp = await axios.get("/api/users/currentuser");
+
+  return {
+    props: {
+      data: resp.data,
+    },
+  };
 }
