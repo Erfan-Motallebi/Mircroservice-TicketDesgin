387f59a9a708302f91620273869d228e6155dee0
Erfan Motallebi <Erfan.Motallebi1990@gmail.com>
2021-12-08 10:19:32 +0330

    refactored the getInitialProps

--- client/helpers/build-client.js
+++ client/helpers/build-client.js
@@ -1,20 +1,19 @@
 import axios from "axios";
 
-export default async function buildClient({ req }) {
-  console.log("api");
+export default function buildClient(req) {
+  let client;
   if (typeof window === "undefined") {
     // Server-Side -> Kubernetes Cluster
-    const client = axios.create({
+    client = axios.create({
       baseURL:
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/",
       headers: req.headers,
     });
-    return client;
   } else {
     // Client-side | Browser-side Request -> Kubernetes Cluster
-    const client = axios.create({
+    client = axios.create({
       baseURL: "/",
     });
-    return client;
   }
+  return client;
 }
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,26 +1,29 @@
 import buildClient from "../helpers/build-client";
 
-function Home() {
-  // console.log({ currentUser });
+function Home({ data: { currentUser } }) {
+  console.log({ currentUser });
   return (
     <div>
       <h1>Landing page</h1>
     </div>
   );
 }
+export default Home;
 
 // export async function getServerSideProps(ctx) {
 //   // const resp = await axios.get("/api/users/currentuser");
-//   console.log("GetServerSide Runs");
-//   return {};
+//   return {
+//      props: {
+//           data: resp.data
+//          }
+//       };
 // }
 
-Home.getInitialProps = async (context) => {
-  const client = buildClient(context);
-  const data = await client.get("/api/users/currentuser");
-  console.log({ client });
-  console.log({ data });
-  return {};
+Home.getInitialProps = async ({ req }) => {
+  const client = buildClient(req);
+  const data = await client.request({
+    method: "GET",
+    url: "/api/users/currentuser",
+  });
+  return { data };
 };
-
-export default Home;
