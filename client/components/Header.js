f84fd54a0696b1d1bbb678ffe4e16e7b0088cdc8
Erfan Motallebi <Erfan.Motallebi1990@gmail.com>
2021-12-10 10:44:04 +0330

    fixed the responsive mode

--- client/components/Header.js
+++ client/components/Header.js
@@ -12,19 +12,9 @@ export default function Header({ currentUser }) {
               </button>
             </a>
           </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
+
           <div
-            className="collapse navbar-collapse d-flex justify-content-end"
+            className=" d-flex justify-content-end"
             id="navbarSupportedContent"
           >
             <ul className="navbar-nav ">
