650b907cba9fa6733fe1c5ac405c1fff2ba3b940
Erfan Motallebi <Erfan.Motallebi1990@gmail.com>
2021-12-14 12:08:03 +0330

    added the ticket model

---
+++ tickets/src/model/ticket.model.ts
@@ -0,0 +1,52 @@
+import { Document, Model, model, Schema } from "mongoose";
+
+interface ITicketAttr {
+  title: string;
+  price: number;
+  userId: string;
+}
+
+interface ITicketDocument extends Document {
+  title: string;
+  pricE: number;
+  userId: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+interface ITicketModel extends Model<ITicketDocument> {
+  build: (attr: ITicketAttr) => ITicketDocument;
+}
+
+const ticketSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      require: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+    versionKey: false,
+  }
+);
+
+ticketSchema.statics.build = (attr: ITicketAttr) => {
+  return new Ticket(attr);
+};
+
+const Ticket = model<ITicketDocument, ITicketModel>("Ticket", ticketSchema);
