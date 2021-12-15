b271ef1d270e11ac81735da6f9eb2b491d4fc8d6
Erfan Motallebi <Erfan.Motallebi1990@gmail.com>
2021-12-15 09:13:55 +0330

    added the show ticket route

---
+++ tickets/src/routes/showTickets.ts
@@ -0,0 +1,15 @@
+import { NotFoundError } from "./../../../common/src/errors/NotFoundError";
+import express, { Router, Request, Response } from "express";
+import Ticket from "../model/ticket.model";
+
+const router: Router = express.Router();
+
+router.get("/api/tickets/:ticketId", async (req: Request, res: Response) => {
+  const { ticketId } = req.params;
+  const ticket = await Ticket.findById(ticketId);
+  if (!ticket) {
+    throw new NotFoundError();
+  }
+
+  res.status(200).json(ticket);
+});
