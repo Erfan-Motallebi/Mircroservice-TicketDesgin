116dd6a52c65933da68ac8a627cdbd34e7302feb
Erfan Motallebi <Erfan.Motallebi1990@gmail.com>
2021-12-18 07:45:25 +0330

    published some data to listen to

--- tickets/nats-test/src/publisher.ts
+++ tickets/nats-test/src/publisher.ts
@@ -6,4 +6,14 @@ const stan = nats.connect("ticketing", "efg", {
 
 stan.on("connect", () => {
   console.log("Connected to the NATS - Client [ STAN ] ");
+
+  const msg = {
+    id: 123,
+    title: "Publish title",
+    price: 0,
+  };
+
+  stan.publish("ticket:created", JSON.stringify(msg), () => {
+    console.log("Data has published.[ Ticket-Created ]");
+  });
 });
